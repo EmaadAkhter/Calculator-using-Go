e64d4fbd175002f8e8ed6137b69c59e1bc63e0af
Emaad_Ansari <67855951+EmaadAkhter@users.noreply.github.com>
2025-08-10 23:10:11 +0530

    Update main.go

--- main.go
+++ main.go
@@ -7,8 +7,8 @@ import (
 )
 
 type eqn struct {
-	Num1     float64 `json:"num1"`
-	Num2     float64 `json:"num2"`
+	Num1     int `json:"num1"`
+	Num2     int `json:"num2"`
 	Operator string  `json:"Operator"`
 }
 
